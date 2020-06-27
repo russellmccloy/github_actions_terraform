9a3c503ebce3a8465ea591cab6a9b7a0fc97e527
Russell McCloy <russell.mccloy@lab3.com.au>
2020-06-27 15:23:39 +1000

    client secret

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@ provider "azurerm" {
   features {}
   subscription_id = "7c610b2a-cbf2-4c68-9e36-2cc5271656b6"
   client_id       = "fc27bbe6-4f03-4d02-814d-f2e2d36354d1"
-  client_secret   = "YFe-f.Uj5c1fFQ4JO2z5-Z_NLQHfffbUJ6"    # TODO: OMG remove this
+  client_secret   = "__CLIENT_SECRET__"    # TODO: OMG remove this
   tenant_id       = "e65107ae-deaa-4f76-b79e-c4b5067a5929"
 }
