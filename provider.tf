199c366bce58b5a083e3adf2257c5a6ec0c9a0a5
Russell McCloy <russell.mccloy@lab3.com.au>
2020-06-27 15:09:51 +1000

    try again

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   version = "~> 2.0"
   features {}
   subscription_id = "7c610b2a-cbf2-4c68-9e36-2cc5271656b6"
-  client_id       = "7d5d81c4-fc41-4357-b93a-88a51679c048"
-  client_secret   = "Kg9KDZ2I17955rh7N0OI~Tj2~yzX~.E.TG"    # TODO: OMG remove this
+  client_id       = "fc27bbe6-4f03-4d02-814d-f2e2d36354d1"
+  client_secret   = "YFe-f.Uj5c1fFQ4JO2z5-Z_NLQHfffbUJ6"    # TODO: OMG remove this
   tenant_id       = "e65107ae-deaa-4f76-b79e-c4b5067a5929"
 }
---
+++ rg.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "github-actions-rg"
+  location = "Australia East"
+}
