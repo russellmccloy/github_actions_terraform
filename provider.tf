5153015ad101baffff3adca0fc315764d10bb0ef
Russell McCloy <russell.mccloy@lab3.com.au>
2020-06-27 14:56:57 +1000

    bad

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@ provider "azurerm" {
   features {}
   subscription_id = "7c610b2a-cbf2-4c68-9e36-2cc5271656b6"
   client_id       = "7d5d81c4-fc41-4357-b93a-88a51679c048"
-  client_secret   = "__CLIENT_SECRET__"
+  client_secret   = "Kg9KDZ2I17955rh7N0OI~Tj2~yzX~.E.TG"    # TODO: OMG remove this
   tenant_id       = "e65107ae-deaa-4f76-b79e-c4b5067a5929"
 }
