42988c682a8440f96dddc047f97ea2dd2195a5b8
Russell McCloy <russell.mccloy@lab3.com.au>
2020-06-27 14:54:35 +1000

    edited wrong proj

--- provider.tf
+++ provider.tf
@@ -1,4 +1,8 @@
 provider "azurerm" {
   version = "~> 2.0"
   features {}
+  subscription_id = "7c610b2a-cbf2-4c68-9e36-2cc5271656b6"
+  client_id       = "7d5d81c4-fc41-4357-b93a-88a51679c048"
+  client_secret   = "__CLIENT_SECRET__"
+  tenant_id       = "e65107ae-deaa-4f76-b79e-c4b5067a5929"
 }
